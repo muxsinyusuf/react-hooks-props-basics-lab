f71f8dfbb5459806e18fa8205d902a010744275d
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-08-16 23:08:58 +0300

    laba done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,34 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+function About(props) {
+  if (props.bio){
+
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+     <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}></Links>
+      
+
+      
     </div>
   );
+}else{
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={user.links.github} linkedin={user.links.linkedin}></Links>
+      
+
+      
+    </div>
+  );
+}
 }
 
 export default About;
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
 const user = {
   name: "Liza",
-  city: "New York",
+  city: "NewYork",
   bio: "I made this!",
   color: "firebrick",
   links: {
